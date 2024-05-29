e6ecf1ea7094d77dd5f214424cceed7f2be4b455
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-05-28 22:18:46 -0400

    set up active state function for the menu buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-const dummy = "dummy";
+
+
+// Toggle active state for each menu button
+$('.menu-button').on('click', function () {
+
+    $('.menu-button').removeClass("menu-button-active");
+    $(this).addClass("menu-button-active");   
+
+    // $(this).toggleClass("menu-button-active");
+
+})
+
+// Show the appropriate sub-content class
+$('#home-button').on('click', function () {
+
+})
