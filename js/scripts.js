b8a23789d01a5128c67f06223b9c3edd74721995
henry-kanengiser <49458585+henry-kanengiser@users.noreply.github.com>
2024-05-28 22:40:01 -0400

    set up menu buttons to toggle sub-content divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
+// Hide sub-content div's until selected by a menu button
+$('.sub-content').hide();
+$('#home').show();
+
 
 
 // Toggle active state for each menu button
 $('.menu-button').on('click', function () {
 
     $('.menu-button').removeClass("menu-button-active");
-    $(this).addClass("menu-button-active");   
+    $(this).addClass("menu-button-active");
 
     // $(this).toggleClass("menu-button-active");
 
@@ -12,5 +16,28 @@ $('.menu-button').on('click', function () {
 
 // Show the appropriate sub-content class
 $('#home-button').on('click', function () {
+
+    // Hide all content
+    $('.sub-content').hide();
+    // Show only the related div
+    $('#home').show();
+
+})
+
+$('#projects-button').on('click', function () {
+
+    // Hide all content
+    $('.sub-content').hide();
+    // Show only the related div
+    $('#projects').show();
+
+})
+
+$('#links-button').on('click', function () {
+
+    // Hide all content
+    $('.sub-content').hide();
+    // Show only the related div
+    $('#links').show();
 
 })
